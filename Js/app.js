9282a60a85eccfe000aced190a2c443e2ed34fea
sebagruia <sebastiangruia@gmail.com>
2019-07-21 21:55:00 +0200

    Edit functionality
    I've added the functionality of the EDIT button

--- Js/app.js
+++ Js/app.js
@@ -1,9 +1,11 @@
+
+//Global Variables
 const input = document.getElementById('input-new-line');
-const newListButton = document.querySelector('.btn-light');
+const insertButton = document.querySelector('.btn-light');
 const todoList = document.querySelector('.todo-list');
 const listWraper = document.querySelector('.list-wraper');
 
-
+// The function that creates the <li> and its HTML content 
 function createList() {
     const li = document.createElement('li');
 
@@ -26,18 +28,22 @@ function createList() {
     checkedIcon.classList.add('far', 'fa-check-square', 'hidden');
     checkedIcon.setAttribute('role', 'button');
 
+    const listText = document.createElement('p');
+    listText.innerHTML = `${input.value}`;
+
     divCheckList.appendChild(unCheckedIcon);
     divCheckList.appendChild(checkedIcon);
-    divCheckList.appendChild(document.createTextNode(input.value));
-    
+    divCheckList.appendChild(listText);
 
     const deleteIcon = document.createElement('i');
     deleteIcon.classList.add('far', 'fa-times-circle');
     deleteIcon.setAttribute('role', 'button');
 
     const editIcon = document.createElement('i');
-    editIcon.classList.add('far', 'fa-edit');
+    editIcon.classList.add('far','fa-edit','btn','btn-primary');
     editIcon.setAttribute('role', 'button');
+    editIcon.setAttribute('data-toggle', 'modal');
+    editIcon.setAttribute('data-target', '#Modal');
 
     divEditList.appendChild(deleteIcon);
     divEditList.appendChild(editIcon);
@@ -45,35 +51,68 @@ function createList() {
     divLi.appendChild(divCheckList);
     divLi.appendChild(divEditList);
 
-    
-
     todoList.appendChild(li);
 
+// The function that creates the functionalitie of the check/uncheck boxes
     function checkOrUnchek() {
         unCheckedIcon.classList.toggle('hidden');
         checkedIcon.classList.toggle('hidden');
     }
-
+// Events for check/uncheck boxes
     unCheckedIcon.addEventListener('click', checkOrUnchek);
     checkedIcon.addEventListener('click', checkOrUnchek);
 
+// The function deletes the selected <li>
     function deleteList() {
         li.remove();
         if(todoList.childElementCount===0){
             listWraper.setAttribute('style', 'border-width:0px');
         }
     }
-
+// Event for delete icon
     deleteIcon.addEventListener('click', deleteList);
+    
+// Event for delete icon
+    editIcon.addEventListener('click', ()=>{
+        if(listText.classList.contains('marked')){}
+        else{
+            const modalInput = document.querySelector('.inputForModal');
+            modalInput.value=listText.innerText;
+            listText.classList.add('marked');
+            
 
-    function editText (){
-        divCheckList.textContent = prompt();
+        }
+
+    });
+
+// Event for edit text box
+    function editText () {
+        const modalInput = document.querySelector('.inputForModal');
+        const markedList = document.querySelector('.marked');
+        if(markedList===null){}
+        else{
+            markedList.innerHTML = `${modalInput.value}`;
+        }
+            
     }
 
-    editIcon.addEventListener('click', editText);
+// Event for Cancel button
+    const modalCancelButton = document.getElementById('modalCancelButton');
+    modalCancelButton.addEventListener('click', ()=>{
+        listText.classList.remove('marked');
+    });
+
+// Event for Save button
+    const modalSaveButton = document.getElementById('modalSaveButton');
+    modalSaveButton.addEventListener('click', ()=>{
+        editText();
+        listText.classList.remove('marked');
+    });
+   
+
 }
 
-
+// The function that adds the created <li> to the <ul>
 function addListToUl() {
         if (input.value.length > 0) {
         createList();
@@ -81,12 +120,13 @@ function addListToUl() {
     }
 }
 
-newListButton.addEventListener('click', () => {
+// Event for Insert button
+insertButton.addEventListener('click', () => {
     addListToUl();
     input.value = '';
 });
 
-
+// Event for <input> using the Enter Key
 input.addEventListener('keypress', (event) => {
     if (input.value.length > 0 && event.keyCode === 13) {
         addListToUl();
